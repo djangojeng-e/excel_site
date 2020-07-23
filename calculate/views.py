51e4b98ee5928b8f75834d8d7d86d5455b837d03
djangojeng-e <headfat1218@gmail.com>
2020-07-23 10:28:44 +0900

    started working on file upload

--- calculate/views.py
+++ calculate/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 
 
 def calculate(request):
-    return HttpResponse("Calculate, calculate function!")
+    file = request.FILES['fileInput']
+    print('# 사용자가 등록한 파일의 이름 : ', file)
+    return render(request, 'calculate/calculate_file_upload.html')
+
