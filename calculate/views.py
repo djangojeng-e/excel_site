042ae188f9f3badecd7502a948f088008d3d74bd
djangojeng-e <headfat1218@gmail.com>
2020-07-24 13:33:48 +0900

    Applied pandas in calculate views.

--- calculate/views.py
+++ calculate/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+import pandas as pd 
 
 # Create your views here.
 
@@ -8,6 +9,8 @@ def calculate(request):
     if request.method == "POST": 
         file = request.FILES['fileInput']
         print("# 사용자가 등록한 파일의 이름 : ",  file)
+        df = pd.read_excel(file, sheet_name='Sheet1', header=0)
+        print(df.head(5))
         return render(request, 'calculate/calculate_file_upload.html')
     else:
         return render(request, 'calculate/calculate_file_upload.html')
