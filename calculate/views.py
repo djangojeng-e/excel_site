f2657448f82640cedbcdf0cd91a2776634bd2345
djangojeng-e <headfat1218@gmail.com>
2020-07-25 12:13:59 +0900

    getting min, max, average value using pandas written in codes.

--- calculate/views.py
+++ calculate/views.py
@@ -11,6 +11,33 @@ def calculate(request):
         print("# 사용자가 등록한 파일의 이름 : ",  file)
         df = pd.read_excel(file, sheet_name='Sheet1', header=0)
         print(df.head(5))
+
+        # grade 별 value 리스트 만들기 
+        grade_dic = {} 
+        total_row_num = len(df.index)
+        for i in range(total_row_num):
+            data = df.loc[i]
+            if not data['grade'] in grade_dic.keys():
+                grade_dic[data['grade']] = [data['value']]
+            else:
+                grade_dic[data['grade']].append(data['value'])
+        
+        # grade 별 최소value 최대value 평균 구하기 
+        grade_calculate_dic = {} 
+        for key in grade_dic.keys():
+            grade_calculate_dic[key] = {}
+            grade_calculate_dic[key]['min'] = min(grade_dic[key])
+            grade_calculate_dic[key]['max'] = max(grade_dic[key])
+            grade_calculate_dic[key]['avg'] = float(sum(grade_dic[key]) / len(grade_dic[key]))
+
+        # 결과 출력 
+        grade_list = list(grade_calculate_dic.keys())
+        grade_list.sort() 
+        for key in grade_list:
+            print("# grade", key)
+            print("min: ", grade_calculate_dic[key]['min'], end='')
+            print("/ max: ", grade_calculate_dic[key]['max'], end='')
+            print("/ avg: ", grade_calculate_dic[key]['avg'], end='\n\n')
         return render(request, 'calculate/calculate_file_upload.html')
     else:
         return render(request, 'calculate/calculate_file_upload.html')
