c1be06dbe58fca031fffbca0235bc9f04589200a
djangojeng-e <headfat1218@gmail.com>
2020-07-26 18:27:58 +0900

    Using pandas, no.of users per email domains

--- calculate/views.py
+++ calculate/views.py
@@ -16,6 +16,7 @@ def calculate(request):
         grade_dic = {} 
         total_row_num = len(df.index)
         for i in range(total_row_num):
+            print(i)
             data = df.loc[i]
             if not data['grade'] in grade_dic.keys():
                 grade_dic[data['grade']] = [data['value']]
@@ -26,9 +27,11 @@ def calculate(request):
         grade_calculate_dic = {} 
         for key in grade_dic.keys():
             grade_calculate_dic[key] = {}
+            print(grade_calculate_dic)
             grade_calculate_dic[key]['min'] = min(grade_dic[key])
             grade_calculate_dic[key]['max'] = max(grade_dic[key])
             grade_calculate_dic[key]['avg'] = float(sum(grade_dic[key]) / len(grade_dic[key]))
+        print(grade_calculate_dic)
 
         # 결과 출력 
         grade_list = list(grade_calculate_dic.keys())
@@ -38,6 +41,19 @@ def calculate(request):
             print("min: ", grade_calculate_dic[key]['min'], end='')
             print("/ max: ", grade_calculate_dic[key]['max'], end='')
             print("/ avg: ", grade_calculate_dic[key]['avg'], end='\n\n')
+        
+        # 이메일 주소 도메인별 인원 구하기 
+        email_domain_dic = {}
+        for i in range(total_row_num):
+            data = df.loc[i]
+            email_domain = (data['email'].split("@"))[1]
+            if not email_domain in email_domain_dic.keys():
+                email_domain_dic[email_domain] = 1
+            else:
+                email_domain_dic[email_domain] += 1
+        print("## EMAIL 도메인별 사용 인원")
+        for key in email_domain_dic.keys():
+            print("#", key, ": ", email_domain_dic[key], "명")
         return render(request, 'calculate/calculate_file_upload.html')
     else:
         return render(request, 'calculate/calculate_file_upload.html')
